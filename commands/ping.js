5ac168d6ddf4e1d3726acd29d81571948bd61d27
itachixbot65 <botitachi226@gmail.com>
2025-03-16 09:17:26 +0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -26,8 +26,8 @@ async function pingCommand(sock, chatId) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '',
+                    newsletterName: 'Hussain Bot',
                     serverMessageId: -1
                 }
             }
